8f359b7747808a3dd0171181006647827f45f30f
Javos <ram16230@uvg.edu.gt>
2019-07-20 20:14:13 -0600

    user class complete

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import slixmpp
+import argparse
 
 if __name__ == '__main__':
-    
+    parser = argparse.ArgumentParser(description='XMPP client.')
+    parser.add_argument('-s', dest='server', help='Server address')
+    parser.add_argument('-j', dest='jid', help='JID to use')
+    parser.add_argument('-p', dest='psw', help='Password')
+
+    args = parser.parse_args()
+
+    if args.server is None:
+        args.server = input('Server address: ')
+    if args.jid is None:
+        args.jid = input('Username: ')
+    if args.psw is None:
+        args.psw = input('Password: ')
+
--- user.py
+++ user.py
@@ -5,10 +5,30 @@ class User(slixmpp.ClientXMPP):
     def __init__(self, jid, password):
         super().__init__(jid, password)
 
+        # start event
         self.add_event_handler('session_start', self.start)
+        # message event
+        self.add_event_handler('message', self.message)
 
     def start(self, event):
+        # <presence />
         self.send_presence()
+        # <iq type=get>
+        #   <query xmlns="data:iq:rooster" />
+        # </iq>
         self.get_roster()
+
+    def message(self, msg):
+        if msg['type'] in ('normal', 'chat'):
+            # Do something
+            print(msg['body'])
+        else:
+            # Error
+            print('Error')
+            print(msg['body'])
+
+
+
+
 
 
